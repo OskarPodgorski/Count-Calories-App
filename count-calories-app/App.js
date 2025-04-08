839a219d57ff76edc5d0ed18317fb5f575dc5ffd
OskarPodgorski <oskar.podgorski.97@gmail.com>
2025-04-08 20:29:16 +0200

    added footer

--- count-calories-app/App.js
+++ count-calories-app/App.js
@@ -4,21 +4,26 @@ import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
+const ColorDarkCyan = "#0E9594";
+const ColorBlack = "#000000";
+const ColorEerieBlack = "#222222";
+
 const Tab = createMaterialTopTabNavigator();
 
 export default function App() {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1 }}>
-      <StatusBar style="dark" backgroundColor="#0E9594" />
+
+      <SafeAreaView style={{ flex: 1 , backgroundColor: ColorEerieBlack}}>
+      <StatusBar style="dark" backgroundColor= {ColorDarkCyan} />
         <NavigationContainer>
           <Tab.Navigator
             screenOptions={{
               tabBarScrollEnabled: true,
               tabBarLabelStyle: { fontSize: 14 },
-              tabBarItemStyle: { width: 75 },
-              tabBarIndicatorStyle: { backgroundColor: '#ff6347' },
-              tabBarStyle: { backgroundColor : "#0E9594"}
+              tabBarItemStyle: { width: 70 },
+              tabBarIndicatorStyle: { backgroundColor: ColorBlack , height: 3 },
+              tabBarStyle: { backgroundColor : ColorDarkCyan}
             }}
             >
             <Tab.Screen name="Mon" component={() => CreateDayScreen('Monday')} />
@@ -31,14 +36,19 @@ export default function App() {
           </Tab.Navigator>
         </NavigationContainer>
       </SafeAreaView>
+      
+      <SafeAreaView style={{ backgroundColor: ColorDarkCyan, height: 70 }}>
+        <Text>FOOTER</Text>
+      </SafeAreaView>
+      
     </SafeAreaProvider>
   );
 }
 
 function CreateDayScreen(dayName) {
   return (
-    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor:"#111111" }}>
-      <Text style={{fontSize: 32, color: "#0E9594"}}>{dayName}</Text>
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: ColorEerieBlack }}>
+      <Text style={{fontSize: 32, color: ColorDarkCyan}}>{dayName}</Text>
     </SafeAreaView>
   );
 }
