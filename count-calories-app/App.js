29c0b9c3bde1df334d224be3c9d202c5fe34bb2d
OskarPodgorski <oskar.podgorski.97@gmail.com>
2025-04-08 16:35:17 +0200

    changed statusbar, background colours

--- count-calories-app/App.js
+++ count-calories-app/App.js
@@ -1,39 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
 const Tab = createMaterialTopTabNavigator();
 
-function CreateDayScreen(dayName) {
-  return (
-    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>{dayName}</Text>
-    </SafeAreaView>
-  );
-}
-
 export default function App() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
+      <StatusBar style="dark" backgroundColor="#0E9594" />
         <NavigationContainer>
           <Tab.Navigator
             screenOptions={{
               tabBarScrollEnabled: true,
-              tabBarLabelStyle: { fontSize: 12 },
-              tabBarItemStyle: { width: 100 },
+              tabBarLabelStyle: { fontSize: 14 },
+              tabBarItemStyle: { width: 75 },
               tabBarIndicatorStyle: { backgroundColor: '#ff6347' },
+              tabBarStyle: { backgroundColor : "#0E9594"}
             }}
-          >
-            <Tab.Screen name="Pon" component={() => CreateDayScreen('Monday')} />
-            <Tab.Screen name="Wt"  component={() => CreateDayScreen('Tuesday')} />
-            <Tab.Screen name="Śr"  component={() => CreateDayScreen('Wednesday')} />
-            <Tab.Screen name="Czw" component={() => CreateDayScreen('Thursday')} />
-            <Tab.Screen name="Pt"  component={() => CreateDayScreen('Friday')} />
-            <Tab.Screen name="Sob" component={() => CreateDayScreen('Saturday')} />
-            <Tab.Screen name="Nd"  component={() => CreateDayScreen('Sunday')} />
+            >
+            <Tab.Screen name="Mon" component={() => CreateDayScreen('Monday')} />
+            <Tab.Screen name="Tue"  component={() => CreateDayScreen('Tuesday')} />
+            <Tab.Screen name="Wed"  component={() => CreateDayScreen('Wednesday')} />
+            <Tab.Screen name="Thu" component={() => CreateDayScreen('Thursday')} />
+            <Tab.Screen name="Fri"  component={() => CreateDayScreen('Friday')} />
+            <Tab.Screen name="Sat" component={() => CreateDayScreen('Saturday')} />
+            <Tab.Screen name="Sun"  component={() => CreateDayScreen('Sunday')} />
           </Tab.Navigator>
         </NavigationContainer>
       </SafeAreaView>
@@ -41,4 +35,10 @@ export default function App() {
   );
 }
 
-
+function CreateDayScreen(dayName) {
+  return (
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor:"#111111" }}>
+      <Text style={{fontSize: 32, color: "#0E9594"}}>{dayName}</Text>
+    </SafeAreaView>
+  );
+}
