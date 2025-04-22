513e2ea1b94afdc18a5e7360ed975617758c0c32
OskarPodgorski <oskar.podgorski.97@gmail.com>
2025-04-22 21:04:43 +0200

    added logo and google logo with login buttons

--- count-calories-app/screens/LoginScreen.js
+++ count-calories-app/screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import { useState, useContext } from 'react';
-import { Text, View, TouchableOpacity, TextInput } from 'react-native';
+import { Text, View, TouchableOpacity, TextInput, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 import * as MyStyles from "../styles/MyStyles"
@@ -9,8 +9,17 @@ export default function LoginScreen(){
     const navigation = useNavigation();
 
     return(
-        <View style={{flex:1, backgroundColor:MyStyles.ColorEerieBlack, justifyContent: "flex-end", alignItems: "center"}}>
-            <TouchableOpacity style={{...MyStyles.baseStyle.base, backgroundColor: MyStyles.ColorDarkCyan, marginBottom: "33%"}}
+        <View style={{flex:1, backgroundColor:MyStyles.ColorEerieBlack, justifyContent: "flex-end", alignItems: "center", gap:10}}>
+
+            <Image source={require("../assets/logo.png")} style={{...MyStyles.baseStyle.base, width:160, height:160, backgroundColor:MyStyles.ColorNight}}/>
+
+            <TouchableOpacity style={{...MyStyles.baseStyle.base, backgroundColor: MyStyles.ColorWhite, flexDirection:"row", alignItems:"flex-end"}}
+            onPress={()=>{navigation.navigate("Main");}}>
+                <Text style={{...MyStyles.baseStyle.text, fontSize:24, paddingRight:5}}>Login with</Text>
+                <Image source={require("../assets/googleLogo.png")} style={{height:37, width:88, marginRight:6}} resizeMode="contain" />
+            </TouchableOpacity>
+
+            <TouchableOpacity style={{...MyStyles.baseStyle.base, backgroundColor: MyStyles.ColorDarkCyan, marginBottom: "40%"}}
             onPress={()=>{navigation.navigate("Main");}}>
                 <Text style={{...MyStyles.baseStyle.text, fontSize:24}}>Enter</Text>
             </TouchableOpacity>
