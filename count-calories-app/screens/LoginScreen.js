14cea8f4c001b04f09c2b33e714db62b2b15559a
OskarPodgorski <oskar.podgorski.97@gmail.com>
2025-04-22 22:48:54 +0200

    Update LoginScreen.js

--- count-calories-app/screens/LoginScreen.js
+++ count-calories-app/screens/LoginScreen.js
@@ -9,20 +9,31 @@ export default function LoginScreen(){
     const navigation = useNavigation();
 
     return(
-        <View style={{flex:1, backgroundColor:MyStyles.ColorEerieBlack, justifyContent: "flex-end", alignItems: "center", gap:10}}>
+        <View style={{flex:1, backgroundColor:MyStyles.ColorEerieBlack, alignItems: "stretch", gap:15}}>
 
-            <Image source={require("../assets/logo.png")} style={{...MyStyles.baseStyle.base, width:160, height:160, backgroundColor:MyStyles.ColorNight}}/>
+            <View style={{flex:1.5,  justifyContent:"center", alignItems: "center", gap:15}}>
 
-            <TouchableOpacity style={{...MyStyles.baseStyle.base, backgroundColor: MyStyles.ColorWhite, flexDirection:"row", alignItems:"flex-end"}}
-            onPress={()=>{navigation.navigate("Main");}}>
-                <Text style={{...MyStyles.baseStyle.text, fontSize:24, paddingRight:5}}>Login with</Text>
-                <Image source={require("../assets/googleLogo.png")} style={{height:37, width:88, marginRight:6}} resizeMode="contain" />
-            </TouchableOpacity>
+                <Image source={require("../assets/logo.png")} style={{...MyStyles.baseStyle.base, width:130, height:130, backgroundColor:MyStyles.ColorNight}}/>
 
-            <TouchableOpacity style={{...MyStyles.baseStyle.base, backgroundColor: MyStyles.ColorDarkCyan, marginBottom: "40%"}}
-            onPress={()=>{navigation.navigate("Main");}}>
-                <Text style={{...MyStyles.baseStyle.text, fontSize:24}}>Enter</Text>
-            </TouchableOpacity>
+                <Text style={{...MyStyles.baseStyle.base, ...MyStyles.baseStyle.text, fontSize:26, color: MyStyles.ColorSilver, backgroundColor: MyStyles.ColorNight}}>Count Calories App</Text>
+
+            </View>
+
+            <View style={{flex:1, justifyContent: "center", alignItems: "center", gap:15}}>
+
+                <TouchableOpacity style={{...MyStyles.baseStyle.base, backgroundColor: MyStyles.ColorWhite, flexDirection:"row", alignItems:"flex-end"}}
+                onPress={()=>{navigation.navigate("Main");}}>
+                    <Text style={{...MyStyles.baseStyle.text, fontSize:24, paddingRight:5}}>Login with</Text>
+                    <Image source={require("../assets/googleLogo.png")} style={{height:37, width:88, marginRight:6}} resizeMode="contain" />
+                </TouchableOpacity>
+
+                <TouchableOpacity style={{...MyStyles.baseStyle.base, backgroundColor: MyStyles.ColorWhite}}
+                onPress={()=>{navigation.navigate("Main");}}>
+                    <Text style={{...MyStyles.baseStyle.text, fontSize:24}}>Enter</Text>
+                </TouchableOpacity>
+
+            </View>
+
         </View>
     );
 }
