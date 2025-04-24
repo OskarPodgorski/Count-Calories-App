20118b4988ff91ce9f28f893330393bd508c15c8
OskarPodgorski <oskar.podgorski.97@gmail.com>
2025-04-24 22:24:54 +0200

    added log in with github button

--- count-calories-app/screens/LoginScreen.js
+++ count-calories-app/screens/LoginScreen.js
@@ -33,8 +33,26 @@ export default function LoginScreen(){
       }
     };
 
+    const handleGitHubSignIn = async () => {
+      try {
+        const { createdSessionId, setActive } = await startSSOFlow({
+          strategy: 'oauth_google',
+          redirectUrl: Linking.createURL('oauth-native-callback'),
+        });
+    
+        console.log(createdSessionId);
+
+        if (createdSessionId) {
+          await setActive({ session: createdSessionId });
+          navigation.navigate("Main");
+        }
+      } catch (err) {
+        console.error('Błąd logowania przez Google:', err);
+      }
+    };
+
     return(
-        <View style={{flex:1, backgroundColor:MyStyles.ColorEerieBlack, alignItems: "stretch", gap:15}}>
+      <View style={{flex:1, backgroundColor:MyStyles.ColorEerieBlack, alignItems: "stretch", gap:15}}>
 
             <View style={{flex:1.5,  justifyContent:"center", alignItems: "center", gap:15}}>
 
@@ -44,13 +62,23 @@ export default function LoginScreen(){
 
             </View>
 
-            <View style={{flex:1, justifyContent: "center", alignItems: "center", gap:15}}>
+            <View style={{flex:1, justifyContent: "center", alignItems: "center"}}>
 
-                <TouchableOpacity style={{...MyStyles.baseStyle.base, backgroundColor: MyStyles.ColorWhite, flexDirection:"row", alignItems:"flex-end"}}
+            <View style={{justifyContent: "center", alignItems: "stretch", gap:15}}>
+
+                <TouchableOpacity style={{...MyStyles.baseStyle.base, backgroundColor: MyStyles.ColorWhite, flexDirection:"row",justifyContent:"center", alignItems:"flex-end"}}
                 onPress={handleGoogleSignIn}>
                     <Text style={{...MyStyles.baseStyle.text, fontSize:24, paddingRight:5}}>Login with</Text>
                     <Image source={require("../assets/googleLogo.png")} style={{height:37, width:88, marginRight:6}} resizeMode="contain" />
                 </TouchableOpacity>
+
+                <TouchableOpacity style={{...MyStyles.baseStyle.base, backgroundColor: MyStyles.ColorWhite, flexDirection:"row", alignItems:"center"}}
+                onPress={handleGitHubSignIn}>
+                    <Text style={{...MyStyles.baseStyle.text, fontSize:24, paddingRight:4}}>Login with</Text>
+                    <Image source={require("../assets/githubLogo.png")} style={{height:43, width:106, marginRight:5 }} resizeMode="contain" />
+                </TouchableOpacity>
+
+            </View>
 
             </View>
 
