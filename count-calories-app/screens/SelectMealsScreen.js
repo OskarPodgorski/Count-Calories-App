b46cc82b9e49914ae90661ed26dfa7ce1e305e5c
OskarPodgorski <oskar.podgorski.97@gmail.com>
2025-05-13 06:21:52 +0200

    Update SelectMealsScreen.js

--- count-calories-app/screens/SelectMealsScreen.js
+++ count-calories-app/screens/SelectMealsScreen.js
@@ -16,31 +16,35 @@ export default function SelectMealsScreen({ route }) {
     <View style={{ flex: 1, backgroundColor: MyStyles.ColorNight }}>
 
       <Text style={{
-        position: "absolute", top: 0, left: 0, right: 0,
+        position: "absolute", top: 0, left: 0, right: 0, zIndex: 1,
         paddingVertical: 12, borderBottomLeftRadius: 24, borderBottomRightRadius: 24, textAlign: "center", fontFamily: MyStyles.BaseFont, fontSize: 28,
         color: MyStyles.ColorWhite, backgroundColor: MyStyles.ColorEerieBlack, elevation: 8
       }}>{`Barcode: ${meals.barcode}\nChoose a product:`}</Text>
 
-      <ScrollView contentContainerStyle={{ paddingTop: 99 + 5, paddingHorizontal: 5, alignItems: "stretch", gap: 10 }}>
+      <ScrollView showsVerticalScrollIndicator={false} showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ paddingTop: 99 + 5, paddingHorizontal: 5, paddingBottom: 10, alignItems: "stretch", gap: 5 }}>
 
         {meals && (
-          <TouchableOpacity style={{ ...MyStyles.baseStyle.base, backgroundColor: MyStyles.ColorOnyx, alignItems: "center", padding: 5 }}>
+          <TouchableOpacity style={{ borderRadius: 16, backgroundColor: MyStyles.ColorOnyx, alignItems: "stretch", padding: 5, elevation: 4 }}>
 
             <Text style={{
               ...MyStyles.baseStyle.base, ...MyStyles.baseStyle.text,
-              backgroundColor: MyStyles.ColorEerieBlack, color: MyStyles.ColorDarkCyan, fontSize: 22, marginHorizontal: 5, marginBottom: 5,
+              backgroundColor: MyStyles.ColorEerieBlack, color: MyStyles.ColorDarkCyan, fontSize: 22, marginHorizontal: 5, marginBottom: 5, alignSelf: "center",
+              elevation: 2
             }}>
               {meals.name}
             </Text>
 
-            <Text style={{ ...MyStyles.baseStyle.text, color: MyStyles.ColorWhite }}>{`Calories: ${meals.calories}`}</Text>
-            <Text style={{ ...MyStyles.baseStyle.text, color: MyStyles.ColorWhite }}>{`Proteins: ${meals.proteins}`}</Text>
-            <Text style={{ ...MyStyles.baseStyle.text, color: MyStyles.ColorWhite }}>{`Fat: ${meals.fat}`}</Text>
-            <Text style={{ ...MyStyles.baseStyle.text, color: MyStyles.ColorWhite }}>{`Carbohydrates: ${meals.carbs}`}</Text>
+            <Text style={{ ...MyStyles.baseStyle.text, color: MyStyles.ColorWhite, fontSize: 18, alignSelf: "center" }}>{`Calories: ${meals.calories}`}</Text>
+
+            <View style={{ flexDirection: "row", justifyContent: "center" }}>
+              <Text style={{ ...MyStyles.baseStyle.text, color: MyStyles.ColorWhite, fontSize: 16 }}>{`Proteins: ${meals.proteins}`}</Text>
+              <Text style={{ ...MyStyles.baseStyle.text, color: MyStyles.ColorWhite, fontSize: 16 }}>{`Fat: ${meals.fat}`}</Text>
+              <Text style={{ ...MyStyles.baseStyle.text, color: MyStyles.ColorWhite, fontSize: 16 }}>{`Carbohydrates: ${meals.carbs}`}</Text>
+            </View>
 
           </TouchableOpacity>
         )}
-
 
       </ScrollView>
     </View>
