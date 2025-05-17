cf9fec7f05f3b3f3445de95ef582df6a5d380797
OskarPodgorski <oskar.podgorski.97@gmail.com>
2025-05-17 04:15:36 +0200

    added add weight button

--- count-calories-app/screens/WeightScreen.js
+++ count-calories-app/screens/WeightScreen.js
@@ -5,6 +5,8 @@ import { useFocusEffect } from '@react-navigation/native';
 
 import { LineChart } from 'react-native-chart-kit';
 
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import Ionicons from '@expo/vector-icons/Ionicons';
 import * as MyStyles from "../styles/MyStyles"
 
 const Tab = createMaterialTopTabNavigator();
@@ -14,19 +16,55 @@ export default function WeightScreen() {
         <View style={{ backgroundColor: MyStyles.ColorNight, flex: 1 }}>
 
             <View style={{
-                position: "absolute", top: 0, left: 0, right: 0, borderBottomLeftRadius: 24, borderBottomRightRadius: 24,
-                backgroundColor: MyStyles.ColorEerieBlack, elevation: 8, overflow: "hidden", alignItems: "center"
+                position: "absolute", top: 0, left: 0, right: 0, borderBottomLeftRadius: 24, borderBottomRightRadius: 24, zIndex: 1,
+                backgroundColor: MyStyles.ColorEerieBlack, overflow: "hidden", alignItems: "center"
             }}>
-                <Text style={{ paddingVertical: 12, textAlign: "center", fontFamily: MyStyles.BaseFont, fontSize: 28, color: MyStyles.ColorWhite }}>Chart</Text>
+                <Text style={{ paddingBottom: 12, textAlign: "center", fontFamily: MyStyles.BaseFont, fontSize: 28, color: MyStyles.ColorWhite }}>Chart</Text>
 
                 <CreateChart />
 
             </View>
 
             <ScrollView showsVerticalScrollIndicator={false} showsHorizontalScrollIndicator={false}
-                contentContainerStyle={{ paddingTop: 99 + 5, paddingHorizontal: 5, paddingBottom: 10, alignItems: "stretch", gap: 5 }}>
+                contentContainerStyle={{ paddingTop: 289 + 5, paddingHorizontal: 5, paddingBottom: 10, alignItems: "stretch", gap: 5 }}>
+
+                <View style={{ ...MyStyles.baseStyle.base, backgroundColor: MyStyles.ColorOnyx, alignItems: "stretch", padding: 5, elevation: 4, flexDirection: "row" }}>
+
+                    <View style={{ flex: 1, overflow: 'hidden', justifyContent: "center" }}>
+                        <Text
+                            style={{ ...MyStyles.baseStyle.text, color: MyStyles.ColorWhite, fontSize: 18 }}
+                            numberOfLines={1}
+                            ellipsizeMode="middle">
+                            Date: Weight:
+                        </Text>
+                    </View>
+
+                    <TouchableOpacity
+                        style={{
+                            ...MyStyles.baseStyle.base,
+                            backgroundColor: MyStyles.ColorEerieBlack,
+                            alignItems: "center",
+                            justifyContent: "center",
+                            alignSelf: "flex-end",
+                            padding: 4,
+                            elevation: 1
+                        }}>
+
+                        <MaterialIcons name="delete-forever" size={30} color={MyStyles.ColorDarkCyan} />
+
+                    </TouchableOpacity>
+
+                </View>
 
             </ScrollView>
+
+            <TouchableOpacity style={{
+                position: "absolute", alignSelf: "center", bottom: 30, width: 70, height: 70, borderRadius: 70 / 2, zIndex: 1, elevation: 2,
+                backgroundColor: MyStyles.ColorEerieBlack, overflow: "hidden", alignItems: "center", justifyContent: "center"
+            }}>
+                <Ionicons name="add-circle" size={40} color={MyStyles.ColorDarkCyan} />
+
+            </TouchableOpacity>
 
         </View>
     );
@@ -41,16 +79,16 @@ function CreateChart() {
             {
                 data: [115, 113, 112, 112, 111, 110, 108],
                 strokeWidth: 3,
-                color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // kolor linii
+                color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`
             },
         ],
-        legend: ["Week Weight"], // optional
+        legend: ["Week Weight"]
     };
 
     const chartConfig = {
         backgroundColor: MyStyles.ColorEerieBlack,
-        backgroundGradientFrom: MyStyles.ColorEerieBlack, // ← to MUSI być ustawione
-        backgroundGradientTo: MyStyles.ColorEerieBlack,   // ← i to też
+        backgroundGradientFrom: MyStyles.ColorEerieBlack,
+        backgroundGradientTo: MyStyles.ColorEerieBlack,
         color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
         labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
         decimalPlaces: 0
